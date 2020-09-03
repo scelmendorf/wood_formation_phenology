e9caccd5d83d12185e74c3d13d84ab0daf7c0ea1
AileneKane <aettinger@fasmail.harvard.edu>
2020-09-03 09:53:08 -0700

    adding color bar legend

--- scripts/gddsims_huang.R
+++ scripts/gddsims_huang.R
@@ -214,5 +214,20 @@ latcol = coltab$latcols[which(coltab$latr ==lat.to.plot)]
  dev.off()
  
  
+ #add color bad legend
+ color.bar <- function(lut, min, max=-min, nticks=11, ticks=seq(min, max, len=nticks), title='') {
+   scale = (length(lut)-1)/(max-min)
+   
+   dev.new(width=1.75, height=5)
+   plot(c(0,1), c(min,max), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='', main=title)
+   axis(2, ticks, las=1)
+   for (i in 1:(length(lut)-1)) {
+     y = (i-1)/scale + min
+     rect(0,y,10,y+1/scale, col=lut[i], border=NA)
+   }	
+ }
+ 
+ color.bar(colorRampPalette(c("#D73027", "#FCA75E", "#CEEAF3", "#4575B4"))(100), min = min(latr), max = max(latr))
+ 
  
  
